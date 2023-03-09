4b0b46ab4ee7bac346d548728bce37bb80f9e044
Henno98 <259554@stud.inn.no>
2023-03-09 16:24:14 +0100

    Created Unreal5 File

---
+++ Suicidal_Penguins/Source/Suicidal_Penguins/Suicidal_Penguins.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Suicidal_Penguins.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Suicidal_Penguins, "Suicidal_Penguins" );
